8c3c3a03ce83c9e02062ccaebbb9f21f7ba0c421
erunzd <23103949@usc.edu.ph>
2025-12-12 07:06:56 +0800

    working map + scrollbar gallery

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -10,9 +10,8 @@ import HomeScreen from '../screens/HomeScreen.js';
 import GlobalScreen from '../screens/GlobalScreen.js';
 import ProfileScreen from '../screens/ProfileScreen.js';
 import PokemonDetailScreen from '../screens/PokemonDetailScreen.js';
-import ARScreen from '../screens/ARScreen.js'; // <-- Keep this one
+import ARScreen from '../screens/ARScreen.js';
 import HuntScreen from '../screens/HuntScreen.js';
-// Removed: import ARScreen from '../screens/ARScreen.js'; // <-- Remove the duplicate
 
 const Stack = createNativeStackNavigator();
 
@@ -27,13 +26,13 @@ const AppNavigator = () => {
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Pokedex" component={HomeScreen} />
 
-        {/* You have two screens registered using the ARScreen component: "AR" and "ARScreen" */}
-        <Stack.Screen name="AR" component={ARScreen} />
-        <Stack.Screen name="Hunt" component={HuntScreen} />                
+        {/* FIXED: Use the correct route name "ARScreen" */}
+        <Stack.Screen name="ARScreen" component={ARScreen} />
+
+        <Stack.Screen name="Hunt" component={HuntScreen} />
         <Stack.Screen name="Global" component={GlobalScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />        
+        <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="PokedexDetail" component={PokemonDetailScreen} />
-        <Stack.Screen name="ARScreen" component={ARScreen} options={{ title: 'AR Encounter' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
